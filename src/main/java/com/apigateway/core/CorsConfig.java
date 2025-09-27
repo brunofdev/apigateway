16aaaff082f96cc71a2aa3640e4e998df585cb8e
Bruno de Fraga <brunofragaa97@gmail.com>
2025-09-27 00:27:33 -0300

    ajustado cors

---
+++ src/main/java/com/apigateway/core/CorsConfig.java
@@ -0,0 +1,33 @@
+package com.apigateway.core;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+
+        // AQUI ESTÁ O AJUSTE:
+        corsConfig.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",      // Para seu desenvolvimento local
+                "https://www.brunofragadev.com" // Para seu site em produção
+        ));
+
+        corsConfig.setMaxAge(3600L);
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfig.addAllowedHeader("*");
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
+}
