d0de9cc47bb5b65f0c0a41d34194f84c49708fe2
Bruno de Fraga <brunofragaa97@gmail.com>
2025-09-28 23:40:19 -0300

    ajustado cors para aceitar requisições de navegadores, requisições OPTIONS

--- src/main/java/com/apigateway/core/SecurityConfig.java
+++ src/main/java/com/apigateway/core/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.apigateway.core;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod; // IMPORTANTE: Adicione este import
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -12,10 +14,12 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http
-                .csrf(ServerHttpSecurity.CsrfSpec::disable) // <-- A LINHA MÁGICA QUE RESOLVE O 403
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchange -> exchange
-                        // Por enquanto, vamos permitir todas as requisições para que nosso
-                        // filtro customizado (AuthenticationFilter) tenha total controle.
+                        // AQUI ESTÁ O AJUSTE:
+                        // Permite a requisição preflight (OPTIONS) do CORS para todas as rotas
+                        .pathMatchers(HttpMethod.OPTIONS).permitAll()
+                        // Mantém a regra anterior para permitir que nosso filtro controle o resto
                         .pathMatchers("/**").permitAll()
                 );
         return http.build();
